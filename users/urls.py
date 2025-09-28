7c8c7dad02d0fef6c731b3cbbd80832ff0f6561a
Almaz <q19.almaz@gmail.com>
2025-09-28 17:14:55 +0500

    added endpoint for getting user id

--- users/urls.py
+++ users/urls.py
@@ -2,7 +2,8 @@ from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from .views import (
     ToolCreatorViewSet, ClientViewSet, UserDetailView, UserAvatarUploadView,
-    ClientRegistrationView, ToolCreatorRegistrationView, AdminRegistrationView
+    ClientRegistrationView, ToolCreatorRegistrationView, AdminRegistrationView,
+    CurrentUserIdView
 )
 
 router = DefaultRouter()
@@ -11,6 +12,7 @@ router.register(r'clients', ClientViewSet, basename='client')
 
 urlpatterns = [
     path('me/', UserDetailView.as_view(), name='user-detail'),
+    path('me/user_id/', CurrentUserIdView.as_view(), name='current-user-id'),
     path('profile/avatar/', UserAvatarUploadView.as_view(), name='profile-avatar-upload'),
 
     path('register/client/', ClientRegistrationView.as_view(), name='client-register'),
